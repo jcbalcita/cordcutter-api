3b81164a3ec477e2f137a0f3c65e7f428a3194d2
Ubuntu <ubuntu@ip-172-31-14-166.us-west-2.compute.internal>
2017-05-17 23:01:10 +0000

    fix typo in elixir version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CordcutterApi.Mixfile do
   def project do
     [app: :cordcutter_api,
      version: "0.0.1",
-     elixir: "~> 1.4444",
+     elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
