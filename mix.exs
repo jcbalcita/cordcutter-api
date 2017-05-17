05c4eb9ea3d4db014ce1d4a2a1732e524ad8df66
Ubuntu <ubuntu@ip-172-31-14-166.us-west-2.compute.internal>
2017-05-17 16:38:26 +0000

    added exrm dependency

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule CordcutterApi.Mixfile do
   def application do
     [mod: {CordcutterApi, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :httpoison]]
+                    :phoenix_ecto, :postgrex, :httpoison, :exrm]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,8 @@ defmodule CordcutterApi.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.11.1"}]
+     {:httpoison, "~> 0.11.1"},
+     {:exrm, "~> 0.14.2"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
