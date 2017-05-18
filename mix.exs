52d526ea5506ee60df136b11ebfd112a28fd48f1
jbalcita <jbalcita@cj.com>
2017-05-18 00:35:18 -0700

    default prod config

--- mix.exs
+++ mix.exs
@@ -38,8 +38,7 @@ defmodule CordcutterApi.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.11.1"},
-     {:distillery, "~> 1.4"}]
+     {:httpoison, "~> 0.11.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
