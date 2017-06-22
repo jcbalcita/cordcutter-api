fc8c338faac5726733fafa94208347e8a9251fce
jbalcita <jbalcita@cj.com>
2017-06-22 09:59:46 -0700

    add skeleton for movie controller test

---
+++ test/controllers/movie_controller_test.exs
@@ -0,0 +1,10 @@
+defmodule CordcutterApi.MovieControllerTest do
+  use CordcutterApi.ConnCase
+
+  # TODO: write unit tests for show and movie controllers
+
+  # test "GET /", %{conn: conn} do
+  #   conn = get conn, "/"
+  #   assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  # end
+end
