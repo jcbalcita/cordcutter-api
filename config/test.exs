bf5a27649cebf13be8dfdaf5b733de6b28b967b6
jbalcita <jbalcita@cj.com>
2017-06-24 19:08:36 -0700

    fetch env vars and set fake modules in test config

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,13 @@ use Mix.Config
 # you can enable the server option below.
 config :cordcutter_api, CordcutterApi.Endpoint,
   http: [port: 4001],
-  server: false
+  server: false,
+  api_key: System.get_env("API_KEY"),
+  base_url: System.get_env("BASE_URL")
+
+config :cordcutter_api,
+  url: CordcutterApi.TestUrl,
+  requester: CordcutterApi.TestRequester
 
 # Print only warnings and errors during test
 config :logger, level: :warn
